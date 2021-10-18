1005729b937a3fb5e7ac2defd194c6a942fec86c
Shamshod <abdushamshod@gmail.com>
2021-10-18 07:49:19 +0500

    feat: mistakes

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const wrapper = document.querySelector('.wrapper')
 const circles = document.querySelectorAll('.circle-block')
 
 
+alert('Извините оказывается я не понял задание и использовал скрипты, дай те ещё 1 или масксимум 2 дня, спасибо за понимание ')
+alert('Извините оказывается я не понял задание и использовал скрипты, дай те ещё 1 или масксимум 2 дня, спасибо за понимание ')
+alert('Извините оказывается я не понял задание и использовал скрипты, дай те ещё 1 или масксимум 2 дня, спасибо за понимание ')
+
 for (let i = 0; i < circles.length; i++){
   circles[i].addEventListener('click', e => {
     removeActiveClass(circles)
